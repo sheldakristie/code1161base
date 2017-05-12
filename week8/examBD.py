d25cd68ce06b9ee543441df99a5e7b3f25bcad8e
atiredturtle <guy@segev.com>
2017-05-12 10:21:50 +1000

    adds new files

--- week8/examBD.py
+++ week8/examBD.py
@@ -1,4 +1,3 @@
-# -*- coding: UTF-8 -*-
 """
 I'm in UR exam.
 
@@ -18,6 +17,7 @@ def greet(name="Towering Timmy"):
     return a string of "Hello" and the name argument.
     E.g. if given as "Towering Timmy" it should return "Hello Towering Timmy"
     """
+    return "Hello " + str(name)
     pass
 
 
@@ -27,6 +27,11 @@ def three_counter(input_list=[1, 4, 3, 5, 7, 1, 3, 2, 3, 3, 5, 3, 7]):
     Return an integer.
     TIP: the test will use a different input_list, so don't just return 5
     """
+    counter = 0
+    for item in input_list:
+        if item == 3:
+            counter += 1
+    return counter
     pass
 
 
@@ -46,6 +51,15 @@ def fizz_buzz():
     """
     fizzBuzzList = []
     # your code here
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            fizzBuzzList.append("FizzBuzz")
+        elif i % 3 == 0:
+            fizzBuzzList.append("Fizz")
+        elif i % 5 == 0:
+            fizzBuzzList.append("Buzz")
+        else:
+            fizzBuzzList.append(i)
     return fizzBuzzList
 
 
@@ -56,6 +70,9 @@ def put_behind_bars(input_string="very naughty boy"):
     e.g. "very naughty boy" should return "|v|e|r|y| |n|a|u|g|h|t|y| |b|o|y|"
     TIP: make sure that you have a pipe on both ends of the string.
     """
+    broken = list(input_string)
+    barred = [x+"|" for x in broken]
+    return "|" + "".join(barred)
     pass
 
 
@@ -69,17 +86,30 @@ def pet_filter(letter="a"):
             "bali cattle", "gayal", "turkey", "goldfish", "rabbit", "koi",
             "canary", "society finch", "fancy mouse", "siamese fighting fish",
             "fancy rat and lab rat", "mink", "red fox", "hedgehog", "guppy"]
+    in_list = []
+    for p in pets:
+        if letter in p:
+            in_list.append(p)
+    return in_list
     pass
 
 
 def best_letter_for_pets():
-    """Return the letter that is present at least once in the most pet names.
+    """Return the letter that is present in the most pet names.
 
     Reusing the pet_filter, find the letter that gives the longest list of pets
     TIP: return just a letter, not the list of animals.
     """
     import string
     the_alphabet = string.lowercase
+    best_letter = ""
+    best_number = 0
+    for letter in the_alphabet:
+        count = len(pet_filter(letter))
+        if count > best_number:
+            best_number = count
+            best_letter = letter
+    return best_letter
     pass
 
 
@@ -98,6 +128,15 @@ def make_filler_text_dictionary():
     TIP: you'll need the requests library
     """
     import requests
+    word_dictionary = {}
+    for number in range(3, 8):
+        word_dictionary[number] = []
+        for word in range(3):
+            url = "http://setgetgo.com/randomword/get.php?len=" + str(number)
+            new_word = requests.get(url).text
+            print(new_word)
+            word_dictionary[number].append(new_word)
+    return word_dictionary
     pass
 
 
@@ -107,13 +146,17 @@ def random_filler_text(number_of_words=200):
     Using the dictionary returned by make_filler_text_dictionary, make a
     paragraph of text using randomly picked words. Each word should be a random
     length, and a random one of the 3 words.
-    Make the paragraph have number_of_words words in it.
-    Return it as a string
     TIP: you'll need the random library
     Bonus: extra mark if you get the paragraph to start with a
            capital letter and end with a full stop.
     """
     import random
+    d = make_filler_text_dictionary()
+    paragraph_list = []
+    for _ in range(number_of_words):
+        w = d[random.randint(3, 7)][random.randint(0, 2)]
+        paragraph_list.append(w)
+    return " ".join(paragraph_list)
     pass
 
 
@@ -129,6 +172,27 @@ def fast_filler(number_of_words=200):
     into and out of the file. Be careful when you read it back in, it'll
     convert integer keys to strings.
     """
+    import os
+    import random
+    import json
+
+    if os.path.isfile("dict_racey.words"):
+        f = open("dict_racey.words", 'r')
+        d = json.loads(f.read())
+        f.close()
+    else:
+        d = make_filler_text_dictionary()
+        f = open("dict_racey.words", 'w')
+        f.write(json.dumps(d))
+        f.close()
+
+    paragraph_list = []
+    for _ in range(number_of_words):
+        number_of_letters = str(random.randint(3, 7))
+        which_word = random.randint(0, 2)
+        w = d[number_of_letters][which_word]
+        paragraph_list.append(w)
+    return " ".join(paragraph_list)
     pass
 
 
@@ -137,9 +201,12 @@ if __name__ == '__main__':
     print(three_counter())
     print(fizz_buzz())
     print(put_behind_bars())
+    print(put_behind_bars("a serial killer"))
+    print(put_behind_bars("a bartender"))
     print(pet_filter())
+    print(pet_filter("p"))
     print(best_letter_for_pets())
-    print(make_filler_text_dictionary())
+    print(len(str(make_filler_text_dictionary())))
     print(random_filler_text())
     print(fast_filler())
     for i in range(10):
--- week8/tests.py
+++ week8/tests.py
@@ -1,57 +1,167 @@
 # -*- coding: UTF-8 -*-
-"""
+"""Run the tests.
+
 This file tests your code. It'll check that the work in each
 of the exercise files does what it's supposed to.
 """
-
 from __future__ import division
 from __future__ import print_function
-import sys
+import imp
 import os
-sys.path.append(os.path.dirname(__file__)[:-5])
-from codeHelpers import test, test_flake8, completion_message
+import sys
+import string
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from codeHelpers import completion_message
+from codeHelpers import ex_runs
+from codeHelpers import nyan_cat
+# from codeHelpers import syntax_error_message
+from codeHelpers import test
+from codeHelpers import test_flake8
+from codeHelpers import Timeout
 
 WEEK_NUMBER = 8
+testResults = []
 
 
-def ex1runs():
+def exam_test(expected,
+              args,
+              function_to_test,
+              finishing_function=None,
+              extra_message=""):
+    print(extra_message)
+    template = ("{n}:\n"
+                "    given:    {a}\n"
+                "    expected: {e}\n"
+                "    got:      {g}\n")
     try:
-        import exercise1
-        return exercise1.function_that_returns_something()
+        got = function_to_test(*args)
+        if finishing_function:
+            print("raw", got)
+            got = finishing_function(got)
+        if len(args) == 0:
+            args = "* no args *"
+        elif len(args) == 1:
+            args = args[0]
+        # else:
+        #     args = args
+        message = template.format(n=function_to_test.__name__,
+                                  a=args,
+                                  e=expected,
+                                  g=got)
+        testResults.append(test(got == expected, message))
     except Exception as e:
-        print("\nThere is a syntax error", str(e))
-        return False
+        message = template.format(n=function_to_test.__name__,
+                                  a=args,
+                                  e=expected,
+                                  g=e)
+        testResults.append(test(False, message))
 
 
-def syntax_error_message(e):
-    print("something went wring with the import.\nProbably a syntax error.")
-    print("does this file run properly on its own?\n" + str(e))
-    return False
+def theTests(path_to_code_to_check="."):
+    """Run all the tests."""
+    print("\nWelcome to the exam!")
+    print("May the odds be ever in your favour.\nEspecially today!")
 
+    ex1path = "{}/week{}/exercise1.py".format(path_to_code_to_check,
+                                              WEEK_NUMBER)
 
-def theTests(path_to_code_to_check=""):
-    print("\nWelcome to week {}!".format(WEEK_NUMBER))
-    print("May the odds be ever in your favour.\n")
+    if ex_runs(path_to_code_to_check, exNumber=1, weekNumber=WEEK_NUMBER):
+        exam = imp.load_source("exercise1", ex1path)
 
-    testResults = []
+        testResults.append(test(test_flake8(ex1path), "pass the linter"))
 
-    # stack the test here #
-    testResults.append(
-        test(test_flake8("week{}/exercise1.py".format(WEEK_NUMBER)),
-             "Exercise 1: pass the linter"))
+        exam_test("Hello the Queen", ["the Queen"], exam.greet)
+        exam_test("Hello Pr♂nc♀♂", ["Pr♂nc♀♂"], exam.greet)
 
-    testResults.append(
-        test(ex1runs(),
-             "Exercise 1: EXPLAIN TEST HERE"))
+        exam_test(4, [[3, 3, 3, 3, 1]], exam.three_counter)
+        exam_test(0, [[0, 1, 2, 5, -9]], exam.three_counter)
+
+        fizza = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11,
+                 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz',
+                 'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29,
+                 'FizzBuzz', 31, 32, 'Fizz', 34, 'Buzz', 'Fizz', 37, 38,
+                 'Fizz', 'Buzz', 41, 'Fizz', 43, 44, 'FizzBuzz', 46, 47,
+                 'Fizz', 49, 'Buzz', 'Fizz', 52, 53, 'Fizz', 'Buzz', 56,
+                 'Fizz', 58, 59, 'FizzBuzz', 61, 62, 'Fizz', 64, 'Buzz',
+                 'Fizz', 67, 68, 'Fizz', 'Buzz', 71, 'Fizz', 73, 74,
+                 'FizzBuzz', 76, 77, 'Fizz', 79, 'Buzz', 'Fizz', 82, 83,
+                 'Fizz', 'Buzz', 86, 'Fizz', 88, 89, 'FizzBuzz', 91, 92,
+                 'Fizz', 94, 'Buzz', 'Fizz', 97, 98, 'Fizz', 'Buzz']
+        exam_test(fizza, [], exam.fizz_buzz)
+
+        exam_test("|a| |s|e|r|i|a|l| |k|i|l|l|e|r|",
+                  ["a serial killer"],
+                  exam.put_behind_bars)
+        exam_test("|a| |b|a|r|t|e|n|d|e|r|",
+                  ["a bartender"],
+                  exam.put_behind_bars)
+
+        exam_test(['red fox'], ["x"], exam.pet_filter)
+        exam_test([], ["q"], exam.pet_filter)
+        exam_test(['pig', 'sheep', 'guinea pig', 'pigeon', 'alpaca', 'guppy'],
+                  ["p"],
+                  exam.pet_filter)
+
+        exam_test("e", [], exam.best_letter_for_pets)
+
+        exam_test(175,
+                  [],
+                  exam.make_filler_text_dictionary,
+                  lambda x: len(str(x)))
+
+        exam_test(True,
+                  [50],
+                  exam.random_filler_text,
+                  lambda x: len(x.split(" ")) == 50 and len(x) > 3*50)
+
+        exam_test(True,
+                  [1000],
+                  exam.random_filler_text,
+                  lambda x: len(x.split(" ")) == 1000 and len(x) > 3*1000)
+
+        exam_test(True,
+                  [100],
+                  exam.fast_filler,
+                  lambda x: len(x.split(" ")) == 100 and len(x) > 3*100)
+        exam_test(True,
+                  [10],
+                  exam.fast_filler,
+                  lambda x: x[0] in string.uppercase and x[1] in string.lowercase,
+                  "Test if fast_filler is capitalised")
+        exam_test(True,
+                  [10],
+                  exam.fast_filler,
+                  lambda x: x[-1] == ".",
+                  "Test if fast_filler finishes with a .")
+
+        #
+        # try:
+        #     with Timeout(1):
+        #         for _ in range(10):
+        #             exam.fast_filler(1000)
+        #         testResults.append(
+        #             test(True,
+        #                  "subsiquent fast_filler"))
+        # except Timeout as t:
+        #     testResults.append(
+        #         test(False,
+        #              "subsiquent fast_filler probably wasn't fast enough"))
+        # except Exception as e:
+        #     testResults.append(
+        #         test(False,
+        #              "subsiquent fast_filler failed" + str(e)))
 
     print("{0}/{1} (passed/attempted)".format(sum(testResults),
                                               len(testResults)))
 
     if sum(testResults) == len(testResults):
-        message = "Rad, you've got all the tests passing!"
+        print(nyan_cat())
+        message = "Cowabunga! You've got all the tests passing!"
         completion_message(message, len(message) + 2)
 
-    return {"of_total": sum(testResults), "mark": len(testResults)}
+    return {"of_total": len(testResults),
+            "mark": sum(testResults),
+            "results": testResults}
 
 
 if __name__ == "__main__":
