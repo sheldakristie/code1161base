60f31c3b4c910960ecb104be18aa199a5a08b9a6
atiredturtle <guy@segev.com>
2017-06-05 14:07:13 +1000

    starts building autocomplete

--- Project/World_internet Use/webPopularity2.js
+++ Project/World_internet Use/webPopularity2.js
@@ -3,15 +3,22 @@ var url = "https://raw.githubusercontent.com/atiredturtle/code1161base/master/Pr
 var CSVdata;
 var dict = {};
 var NUM_FAVS = 5
-
+var website_to_draw = "www.facebook.com";
 
 function drawRegionsMap() {
-
-    var data = google.visualization.arrayToDataTable(generateArray(dict));
+    // var nameValue = document.getElementById("uniqueID").value;
+    // console.log(nameValue);
+    var data = google.visualization.arrayToDataTable(generateArray(dict, website_to_draw));
 
     var options = {
         tooltip: {
             isHtml: true
+        },
+        sizeAxis:{
+            maxValue: 50
+        },
+        colorAxis: {
+            colors: ['#00FF00', '#FF0000']
         }
     };
 
@@ -32,7 +39,6 @@ function drawRegionsMap() {
 }
 
 
-
 Papa.parse(url, {
     download: true,
     header: true,
@@ -51,6 +57,23 @@ Papa.parse(url, {
     }
 });
 
+new autoComplete({
+    selector: 'input[name="websiteSearch"]',
+    minChars: 1,
+    source: function(term, suggest){
+        term = term.toLowerCase();
+        var choices = ['ActionScript', 'AppleScript', 'Asp'];
+        var matches = [];
+        for (i=0; i<choices.length; i++)
+            if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
+        suggest(matches);
+    },
+    renderItem: function (item, search){
+        search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+        var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
+        return '<div class="autocomplete-suggestion" data-val="' + item + '">' + item.replace(re, "<b>$1</b>") + '</div>';
+    }
+});
 
 function processData(d){
     for (var i = 0; i < d.length; i++){
@@ -64,34 +87,28 @@ function processData(d){
     }
 }
 
-function generateArray(d){
+function generateArray(d, searchSite){
     var arr = [];
-    var header = [{id:'Country', type:'string'}, {type:'string', role:'tooltip', p:{html:true}}];
-
-    console.log(header);
+    var header = ['Country', 'Popularity'];
+    
     arr.push(header);
     for (var key in d) {
         var subArr = [];
         subArr.push(key);
-        var websites = "<ol>";
-        for (var i = 0; i < NUM_FAVS; i++){
-            websites += "<li>";
-            websites += d[key][i];
-            websites += "</li>"
+        var popularity = null; // defaults 
+        for (var i = 0; i < d[key].length; i++){
+            if (d[key][i] == searchSite){
+                popularity = i+1;
+                subArr.push(popularity);
+                arr.push(subArr);
+            }
         }
-        websites += "</ol>";
-        subArr.push(websites);
-        arr.push(subArr);
     }
-    console.log(arr);
-
     return arr;
 }
 
-
-
-
-
-
-
-
+$('#websiteSearch').change(function(){
+    console.log("www."+ $('#websiteSearch').val()+".com");
+    website_to_draw = "www."+ $('#websiteSearch').val()+".com";
+    drawRegionsMap()
+});
